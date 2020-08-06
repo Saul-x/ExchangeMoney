eb15b8db6375b77b45d583909fbaecd9e72a851a
EternalSaul <eternx@163.com>
2020-08-07 01:47:00 +0800

    feat: add method exchangeMoneyWithMostCurrencies

--- src/main/java/com/thoughtworks/money/CurrenciesExchangeUtils.java
+++ src/main/java/com/thoughtworks/money/CurrenciesExchangeUtils.java
@@ -1,8 +1,19 @@
 package com.thoughtworks.money;
 
+import java.util.Comparator;
+import java.util.stream.Stream;
+
 public class CurrenciesExchangeUtils {
     public static String exchangeMoneyWithLeastCurrencies(int num, Currency unit) {
         ExchangeSolution exchange = new ExchangeSolution(num * unit.value).exchange();
         return num + unit.name + "= " + exchange.detailsDescription();
+    }
+
+    public static String exchangeMoneyWithMostCurrencies(int num, Currency unit) {
+        if (num == 0) return "";
+        return Stream.of(Currency.values())
+                .min(Comparator.comparing(currency -> currency.value))
+                .map(cheapest -> num + unit.name + "= " + num * unit.value / cheapest.value + cheapest.name)
+                .orElse("");
     }
 }
