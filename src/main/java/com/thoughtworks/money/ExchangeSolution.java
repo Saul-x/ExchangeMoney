b5893c825562b160bda78ea73a31df0baae26318
EternalSaul <eternx@163.com>
2020-08-06 23:44:04 +0800

    refactoring: rm some useless var

--- src/main/java/com/thoughtworks/money/ExchangeSolution.java
+++ src/main/java/com/thoughtworks/money/ExchangeSolution.java
@@ -3,9 +3,16 @@ package com.thoughtworks.money;
 import java.util.HashMap;
 
 public class ExchangeSolution {
+    int total;
     HashMap<String, Integer> details;
-    public ExchangeSolution(HashMap<String, Integer> details) {
+
+    public ExchangeSolution(int total, HashMap<String, Integer> details) {
+        this.total = total;
         this.details = details;
+    }
+
+    public HashMap<String, Integer> getDetails() {
+        return details;
     }
 
     public int currenciesCount() {
--- src/main/java/com/thoughtworks/money/MoneyExchange.java
+++ src/main/java/com/thoughtworks/money/MoneyExchange.java
@@ -4,28 +4,31 @@ import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
+import java.util.stream.Collectors;
+import java.util.stream.IntStream;
 
 public class MoneyExchange {
     public static ExchangeSolution exchange(int total, Map<Integer, String> moneyValues) {
         List<Integer> moneyKinds = new ArrayList<>(moneyValues.keySet());
-        ExchangeSolution[] exchangeSolutions = new ExchangeSolution[total + 1];
-        exchangeSolutions[0] = new ExchangeSolution(new HashMap<>());
-        for (int currentTotal = 1; currentTotal < exchangeSolutions.length; currentTotal++) {
-            int minMoneyCount = Integer.MAX_VALUE;
-            int optimizeKind = 0;
-            for (int index = 0; index < moneyKinds.size(); index++) {
-                if (currentTotal >= moneyKinds.get(index)) {
-                    if (minMoneyCount > exchangeSolutions[currentTotal - moneyKinds.get(index)].currenciesCount()) {
-                        minMoneyCount = exchangeSolutions[currentTotal - moneyKinds.get(index)].currenciesCount();
-                        optimizeKind = index;
+        final List<ExchangeSolution> exchangeSolutions = IntStream.rangeClosed(0, total).mapToObj(amount -> new ExchangeSolution(amount, new HashMap<>()))
+                .collect(Collectors.toList());
+        exchangeSolutions.stream()
+                .filter(exchangeSolution -> exchangeSolution.total > 0)
+                .forEach(exchangeSolution -> {
+                    int minMoneyCount = Integer.MAX_VALUE;
+                    int optimizeKind = 0;
+                    for (int index = 0; index < moneyKinds.size(); index++) {
+                        if (exchangeSolution.total >= moneyKinds.get(index)) {
+                            if (minMoneyCount > exchangeSolutions.get(exchangeSolution.total - moneyKinds.get(index)).currenciesCount()) {
+                                minMoneyCount = exchangeSolutions.get(exchangeSolution.total - moneyKinds.get(index)).currenciesCount();
+                                optimizeKind = index;
+                            }
+                        }
                     }
-                }
-            }
-            HashMap<String, Integer> details = (HashMap<String, Integer>) exchangeSolutions[currentTotal - moneyKinds.get(optimizeKind)].details.clone();
-            details.computeIfPresent(moneyValues.get(moneyKinds.get(optimizeKind)), (key, value) -> ++value);
-            details.putIfAbsent(moneyValues.get(moneyKinds.get(optimizeKind)), 1);
-            exchangeSolutions[currentTotal] = new ExchangeSolution(details);
-        }
-        return exchangeSolutions[total];
+            exchangeSolution.getDetails().putAll(exchangeSolutions.get(exchangeSolution.total - moneyKinds.get(optimizeKind)).details);
+            exchangeSolution.getDetails().computeIfPresent(moneyValues.get(moneyKinds.get(optimizeKind)), (key, value) -> ++value);
+            exchangeSolution.getDetails().putIfAbsent(moneyValues.get(moneyKinds.get(optimizeKind)), 1);
+        });
+        return exchangeSolutions.get(total);
     }
 }
